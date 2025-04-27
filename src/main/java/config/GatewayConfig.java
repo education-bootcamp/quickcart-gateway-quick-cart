29cecb561b0a51eaf842200fc130c51749a8486e
education-bootcamp <education.seekerscloud@gmail.com>
2025-04-27 21:47:48 +0530

    3

--- src/main/java/config/GatewayConfig.java
+++ src/main/java/config/GatewayConfig.java
@@ -4,6 +4,8 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class GatewayConfig {
